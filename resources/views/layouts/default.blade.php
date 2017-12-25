3ffde986730dc1d51467ab53abe450799591f2bb
vagrant <vagrant@homestead>
2017-12-25 11:54:31 +0000

    Finish layout and routes

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title','sample')</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+  @include('layouts._header')
+<div class=container>
+ @yield('content')
+ @include('layouts._footer')
+</div>
+
+</body></html>
